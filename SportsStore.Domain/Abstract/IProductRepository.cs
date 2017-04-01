c1b20864ed6140ebdd5611e545cf1ab89d692510
nlivaic <nlivaic@gmail.com>
2017-04-01 19:16:23 +0200

    Initial commit.

---
+++ SportsStore.Domain/Abstract/IProductRepository.cs
@@ -0,0 +1,8 @@
+﻿using SportsStore.Domain.Entities;
+using System.Linq;
+
+namespace SportsStore.Domain.Abstract {
+    public interface IProductRepository {
+        IQueryable<Product> Products { get; }
+    }
+}
---
+++ SportsStore.WebUI/Controllers/ProductController.cs
@@ -0,0 +1,20 @@
+﻿using SportsStore.Domain.Abstract;
+using System.Web.Mvc;
+
+namespace SportsStore.WebUI.Controllers {
+    public class ProductController : Controller
+    {
+        private IProductRepository repository;
+
+        public ProductController(IProductRepository productRepository) {
+            repository = productRepository;
+        }
+        
+        // GET: Product
+        public ViewResult List()
+        {
+            System.Linq.IQueryable<Domain.Entities.Product> foo = repository.Products;
+            return View(foo);
+        }
+    }
+}
