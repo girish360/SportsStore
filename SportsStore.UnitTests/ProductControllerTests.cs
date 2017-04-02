3c0f22de3af123e91788c045fa7cc655e721af35
nenad <nenad@DESKTOP-EL2GKTS>
2017-04-02 10:40:54 +0200

    Test pagination:  - paging view model  - paging helper method

--- SportsStore.UnitTests/ProductControllerTests.cs
+++ SportsStore.UnitTests/ProductControllerTests.cs
@@ -5,6 +5,9 @@ using Moq;
 using System.Linq;
 using SportsStore.WebUI.Controllers;
 using System.Collections.Generic;
+using SportsStore.WebUI.HtmlHelpers;
+using System.Web.Mvc;
+using SportsStore.WebUI.Models;
 
 namespace SportsStore.UnitTests {
     [TestClass]
@@ -24,12 +27,54 @@ namespace SportsStore.UnitTests {
 
             // Act
             target.PageSize = 3;
-            Product[] prodArray = ((IEnumerable<Product>)target.List(2).Model).ToArray<Product>();
+            ProductListViewModel viewModel = (ProductListViewModel)target.List(2).Model;
+            Product[] prodArray = ((IEnumerable<Product>)viewModel.Products).ToArray<Product>();
 
             // Assert
             Assert.IsTrue(prodArray.Length == 2);
             Assert.IsTrue(prodArray[0].ProductID == 4);
             Assert.IsTrue(prodArray[1].ProductID == 5);
+        }
+
+        [TestMethod]
+        public void Can_Generate_Paging_Links() {
+            // Arrange
+            HtmlHelper myHelper = null;
+            PagingInfo pagingInfo = new PagingInfo {
+                CurrentPage = 2,
+                TotalItems = 28,
+                ItemsPerPage = 10
+            };
+
+            // Act
+            MvcHtmlString result = myHelper.PageLinks(pagingInfo, (i => "Page" + i));
+
+            // Assert
+            Assert.AreEqual(result.ToString(), @"<a href=""Page1"">1</a><a class=""selected"" href=""Page2"">2</a><a href=""Page3"">3</a>");
+        }
+
+        [TestMethod]
+        public void Can_Send_Pagination_Data() {
+            // Arrange
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new Product[] {
+                new Product { ProductID = 1, Name = "P1"},
+                new Product { ProductID = 2, Name = "P2"},
+                new Product { ProductID = 3, Name = "P3"},
+                new Product { ProductID = 4, Name = "P4"},
+                new Product { ProductID = 5, Name = "P5"},
+            }.AsQueryable());
+            ProductController target = new ProductController(mock.Object);
+            target.PageSize = 3;
+
+            // Act
+            ProductListViewModel model = (ProductListViewModel)target.List(2).Model;
+
+            // Assert
+            Assert.IsTrue(model.PagingInfo.CurrentPage == 2);
+            Assert.IsTrue(model.PagingInfo.ItemsPerPage == 3);
+            Assert.IsTrue(model.PagingInfo.TotalItems == 5);
+            Assert.IsTrue(model.PagingInfo.TotalPages == 2);
         }
     }
 }
