5ba17f8692843ca68fe8a13f3611dbf05856d158
nenad <nenad@DESKTOP-EL2GKTS>
2017-04-07 20:08:31 +0200

    Add new domain entity: Shopping Cart.

---
+++ SportsStore.Domain/Entities/Cart.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SportsStore.Domain.Entities {
+    public class Cart {
+        private List<CartLine> lineCollection = new List<CartLine>();
+
+        public void AddItem(Product product, int quantity) {
+            CartLine item = lineCollection.Where(p => p.Product.ProductID == product.ProductID).FirstOrDefault<CartLine>();
+            if (item != null) {
+                item.Quantity += quantity;
+            } else {
+                lineCollection.Add(new CartLine {
+                    Product = product,
+                    Quantity = quantity
+                });
+            }
+        }
+
+        public void RemoveLine(Product product) {
+            lineCollection.RemoveAll(l => l.Product.ProductID == product.ProductID);
+        }
+
+        public decimal ComputeTotalValue() {
+            return lineCollection.Sum(l => l.Quantity);
+        }
+
+        public void Clear() {
+            lineCollection.Clear();
+        }
+
+        public IEnumerable<CartLine> Lines { get { return lineCollection; } }
+    }
+
+    public class CartLine {
+        public Product Product { get; set; }
+        public int Quantity { get; set; }
+    }
+}
